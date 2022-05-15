d658f215bcaf683b29ec9c2093201d301c0237d6
Veranika Kasparevych <veranikabarel@gmail.com>
2022-05-15 19:46:55 +0200

    feat: create add item functionality

--- app.js
+++ app.js
@@ -7,3 +7,35 @@ list.addEventListener('click', function (e) {
         list.removeChild(li);
     }
 })
+
+// ADD THE ITEM
+const addForm = document.forms['add-item'];
+
+addForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+
+    // CREATE THE ELEMENT 
+
+    const li = document.createElement('li');
+    const itemName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    // ADD CONTENT
+
+    deleteBtn.textContent = 'delete';
+    itemName.textContent = value;
+
+    // ADD CLASSES
+
+    itemName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
+    // APPEND TO DOM
+
+    li.appendChild(itemName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+})
+
+
